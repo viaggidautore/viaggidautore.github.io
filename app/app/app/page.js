6f2a4475890d53553564d3a412066113658a6c43
viaggidautore <paponelab@gmail.com>
2026-03-07 16:58:06 +0100

    Delete app/app/app/page.js

--- app/app/app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div style={{ fontSans: 'sans-serif', padding: '20px' }}>
-      <h1>Viaggi d'Autori - Chat in arrivo</h1>
-      <p>Stiamo configurando la tua chat con Gemini...</p>
-    </div>
-  );
-}
